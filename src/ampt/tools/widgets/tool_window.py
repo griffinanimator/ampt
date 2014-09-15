9a0d1e6a236ee386887d85967a128b911b78a206
Michael Trainor <accounts@mtrainor.com>
2014-09-15 15:38:05 -0700

    removed maya related code
    this supports the separation of the code bases

--- src/ampt/tools/widgets/tool_window.py
+++ src/ampt/tools/widgets/tool_window.py
@@ -1,25 +1,12 @@
 from PySide import QtCore, QtGui
-from ...core.user_interface import get_maya_main_window
 
 
 class ToolWindow(QtGui.QMainWindow):
-    def __init__(self, title="Untitled"):
-        maya_main_window = get_maya_main_window()
+    def __init__(self, parent=None):
 
-        def flush():
-            for i in maya_main_window.children():
-                cls = type(i).__name__
-                if cls == self.__class__.__name__:
-                    i.setParent(None)
-                    i.destroy()
-
-        def build():
-            super(ToolWindow, self).__init__(parent=maya_main_window)
-            self.title = title
-            self.dimensions = QtCore.QRect(0, 0, 1280, 720)
-
-        flush()
-        build()
+        super(ToolWindow, self).__init__(parent=parent)
+        self.title = "Untitled"
+        self.dimensions = QtCore.QRect(0, 0, 1280, 720)
 
     def display(self):
         self.setWindowTitle(self.title)
@@ -27,10 +14,28 @@ class ToolWindow(QtGui.QMainWindow):
         self.move(self.screen_center)
         self.show()
 
-
     @property
     def screen_center(self):
         screen = QtGui.QDesktopWidget().screenGeometry()
         x = (screen.width() - self.width()) * 0.5
         y = (screen.width() - self.width()) * 0.25
         return QtCore.QPoint(x, y)
+
+
+# Maya Code
+# maya_main_window = get_maya_main_window()
+#
+# def flush():
+#     for i in maya_main_window.children():
+#         cls = type(i).__name__
+#         if cls == self.__class__.__name__:
+#             i.setParent(None)
+#             i.destroy()
+
+# Test Script
+# if __name__ == "__main__":
+#     import sys
+#     app = QtGui.QApplication(sys.argv)
+#     test = ToolWindow()
+#     test.display()
+#     sys.exit(app.exec_())
