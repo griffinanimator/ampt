2798cedaf131268ea8e50c23e434b2008f601587
Michael Trainor <accounts@mtrainor.com>
2014-09-15 17:09:20 -0700

    added tree data

--- src/ampt/tools/camel/animation_library/animation_category_widget.py
+++ src/ampt/tools/camel/animation_library/animation_category_widget.py
@@ -1,4 +1,31 @@
-from PySide import QtGui
+from PySide import QtCore, QtGui
+
+
+def fill_tree(item, value):
+    item.setExpanded(True)
+    if type(value) is dict:
+        for key, val in sorted(value.iteritems()):
+            child = QtGui.QTreeWidgetItem()
+            child.setText(0, unicode(key))
+            item.addChild(child)
+            fill_tree(child, val)
+    elif type(value) is list:
+        for val in value:
+            child = QtGui.QTreeWidgetItem()
+            item.addChild(child)
+            if type(val) is dict:
+                child.setText(0, '[dict]')
+                fill_tree(child, val)
+            elif type(val) is list:
+                child.setText(0, '[list]')
+                fill_tree(child, val)
+            else:
+                child.setText(0, unicode(val))
+            child.setExpanded(True)
+    else:
+        child = QtGui.QTreeWidgetItem()
+        child.setText(0, unicode(value))
+        item.addChild(child)
 
 
 class AnimationCategoryWidget(QtGui.QWidget):
@@ -6,19 +33,20 @@ class AnimationCategoryWidget(QtGui.QWidget):
         super(AnimationCategoryWidget, self).__init__(parent=parent)
 
         layout = QtGui.QVBoxLayout(self)
+        self.tree = QtGui.QTreeWidget(self)
 
-        tree = QtGui.QTreeWidget(self)
-        tree.setColumnCount(1)
+        self.tree.setHeaderHidden(True)
+        self.tree.adjustSize()
 
-        items = []
-
-        for i in range(10):
-            items.append(QtGui.QTreeWidgetItem(["item: %s" % i]))
-
-        tree.insertTopLevelItems(0, items)
-        tree.adjustSize()
-
-        layout.addWidget(tree)
+        layout.addWidget(self.tree)
 
         self.setLayout(layout)
         self.update()
+
+    def set_data(self, data):
+        self.update_data(data)
+        self.update()
+
+    def update_data(self, data):
+        self.tree.clear()
+        fill_tree(self.tree.invisibleRootItem(), data)
