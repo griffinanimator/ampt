dbbad9650ecb7a4730436e1eff1a8f213460e98c
Michael Trainor <accounts@mtrainor.com>
2014-09-22 00:47:08 -0700

    added selection change callback

--- src/ampt/tools/animation_library/__init__.py
+++ src/ampt/tools/animation_library/__init__.py
@@ -1,36 +1,33 @@
-from ampt.core.user_interface import get_maya_main_window
+from ampt.core.user_interface import load_interface
 from ampt.tools.content_library import ContentLibrary
 
 import maya.OpenMaya as OpenMaya
 import pymel.core as pm
+
+from PySide import QtCore
 
 
 class AnimationLibrary(ContentLibrary):
     def __init__(self, parent=None):
         super(AnimationLibrary, self).__init__(parent=parent)
 
+        self.title = "Animation Library"
+        self.outliner.set_data(pm.ls(dag=True))
+        self.set_selection_callback()
+
+    def set_selection_callback(self):
+        def callback(_):
+            selected_obj = pm.selected(type="transform")
+            selected_item = self.outliner.tree.findItems(selected_obj[0].name(), QtCore.Qt.MatchExactly)[0]
+            self.selection_changed.emit(selected_obj)
+            self.outliner.set_current_item(selected_item)
+
+        OpenMaya.MEventMessage.addEventCallback("SelectionChanged", callback)
+
 
 def load():
     try:
-        maya_main_window = get_maya_main_window()
-
-        for i in maya_main_window.children():
-            cls = type(i).__name__
-            if cls == "AnimationLibrary":
-                i.setParent(None)
-                i.destroy()
-
-        animation_library = AnimationLibrary(maya_main_window)
-
-        def on_selection_changed(_):
-            animation_library.selection_changed.emit(pm.selected(type="transform"))
-            animation_library.outliner.set_current_item(pm.selected(type="transform"))
-
-        OpenMaya.MEventMessage.addEventCallback("SelectionChanged", on_selection_changed)
-
-        animation_library.category_widget.set_data(pm.ls(dag=True))
-        animation_library.display()
-
+        load_interface(None, AnimationLibrary)
     except Exception as e:
         print e.message
         pass
