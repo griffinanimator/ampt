049552b11fa030d21d7b11a1914010764a8c0077
Michael Trainor <accounts@mtrainor.com>
2014-09-25 10:17:08 -0700

    added qt selection callback

--- src/ampt/tools/animation_library/__init__.py
+++ src/ampt/tools/animation_library/__init__.py
@@ -1,5 +1,5 @@
 from ampt.core.user_interface import load_interface
-from ampt.tools.content_library import ContentLibrary
+from ampt.tools.widgets.content_library import ContentLibrary
 
 import maya.OpenMaya as OpenMaya
 import pymel.core as pm
@@ -16,13 +16,20 @@ class AnimationLibrary(ContentLibrary):
         self.set_selection_callback()
 
     def set_selection_callback(self):
-        def callback(_):
+        def om_callback(_):
             selected_obj = pm.selected(type="transform")
             selected_item = self.outliner.tree.findItems(selected_obj[0].name(), QtCore.Qt.MatchExactly)[0]
             self.selection_changed.emit(selected_obj)
             self.outliner.set_current_item(selected_item)
 
-        OpenMaya.MEventMessage.addEventCallback("SelectionChanged", callback)
+        OpenMaya.MEventMessage.addEventCallback("SelectionChanged", om_callback)
+
+        def qt_callback(*args, **kwargs):
+            item = self.outliner.tree.currentItem().text(0)
+            if len(item):
+                pm.select(item, r=True)
+
+        self.outliner.tree.itemSelectionChanged.connect(qt_callback)
 
 
 def load():
