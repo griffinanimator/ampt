f8dbdeab5c93d1161b7934fac6d750e99a80861d
Michael Trainor <accounts@mtrainor.com>
2015-05-14 13:52:35 -0700

    working on component data format
    the elements property should always return a complete ordered list/dict
    of the component elements. attributes may need it's instance objects if
    they start to drive any logic

--- src/ampt/tools/modular_rigging/component.py
+++ src/ampt/tools/modular_rigging/component.py
@@ -1,5 +1,6 @@
 # standard libraries
 import os
+import collections
 
 # internal libraries
 from ampt.data.dict_utils import JSONDict
@@ -12,14 +13,59 @@ COMPONENTS_PATH = os.path.join(MODULE_PATH, "components").replace("\\", "/")
 
 
 class Component(object):
-    def __init__(self, data, parent=None, child=None):
-        self.data = data
+    def __init__(self, data, parent=None, children=None):
+        self.data = data or dict()
+        self.parent = parent
+        self.children = children or list()
 
-    def install(self):
-        print "fuck"
-        for name, position in self.data["layout"]["controls"].iteritems():
+    @property
+    def has_parent(self):
+        return True if self.parent else False
 
-            # create component nodes
-            pm.select(clear=True)
-            component_node = pm.createNode("component_contol_sphere", name = name+"_component")
-            pm.select(clear=True)
+    @property
+    def has_children(self):
+        if self.children:
+            if isinstance(self.children, list) or isinstance(self.children, tuple):
+                return True if len(self.children) else False
+        return False
+
+    @property
+    def name(self):
+        _name = self.data["name"] or None
+        return _name if _name else None
+
+    @property
+    def layout(self):
+        layout = self.data["layout"] or None
+        return layout if layout else None
+
+    @property
+    def head(self):
+        _head = self.layout["head"] if self.layout else None
+        return ("head", _head) if _head else None
+
+    @property
+    def tail(self):
+        _tail = self.layout["tail"] if self.layout else None
+        return ("tail", _tail) if _tail else None
+
+    @property
+    def elements(self):
+        _elements = collections.OrderedDict()
+        if self.head and self.tail:
+            for key, value in self.layout.iteritems():
+                if key not in ["head", "tail"]:
+                    index = value["index"]
+                    print "foo: ", index
+                    _elements[index+1] = (key, value)
+        _elements[0] = self.head
+        _elements[len(_elements) + 1] = self.tail
+        return _elements
+
+    def dump_data(self, file_path):
+        output = JSONDict(file_path)
+        output.update(self.data)
+
+    def create_component_node(self):
+        print self.elements
+
